571f275fde0b3922191075506697adc571fcb90f
Shane Doyle <shanedoyle1989@gmail.com>
2023-09-25 13:20:15 +0100

    feat(assets/js/main.js): select DOM elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
 import tileData from "./tileData.js";
 import { shuffle, getRandomNumber } from "./utils.js";
+
+// ---------------------
+// DOM Selectors
+// ---------------------
+
+const menuContent = document.querySelector(".menu-content");
+const menuFrame = document.querySelector(".menu-frame");
+const gridContainer = document.querySelector(".grid-container");
+const grid = document.querySelector(".grid");
+const playerLivesDisplay = document.querySelector(".player-lives-display");
+const timeRemaining = document.querySelector(".time-remaining");
+const interactiveMenu = document.querySelector(".interactive-menu");
+const interactiveMenuContent = document.querySelector(
+  ".interactive-menu-content"
+);
+const menuButtons = document.querySelectorAll(".menu-button");
+const instructionsText = document.querySelector(".instructions-text");
+const instructionsButton = document.querySelector(".instructions-button");
+const instructionsBackButton = document.querySelector(
+  ".instructions-back-button"
+);
+const gameBackButton = document.querySelector(".back-button");
